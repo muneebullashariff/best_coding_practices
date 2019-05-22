36167727692acd31e82e84c08de328f25aaef024
Muneeb Ulla Shariff <muneebullashariff@gmail.com>
2019-05-22 10:20:35 +0530

    Create git_github_guidelines.sv

---
+++ git_github_guidelines.sv
@@ -0,0 +1,5 @@
+Always use the branch to do the development work and once everything works on the development-branch go ahead and merge the changes
+into the master-branch. DON'T MESS WITH THE MASTER
+
+Please refer the article below as how to achieve this:
+https://thenewstack.io/dont-mess-with-the-master-working-with-branches-in-git-and-github/
